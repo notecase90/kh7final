e2cc0c967659e844fd98b9a78b588ea995ef3922
NAeeeee <zzz0525z@naver.com>
2021-07-16 14:22:34 +0900

    리뷰

---
+++ springfinal/src/main/java/com/kh/project/repository/ReviewDao.java
@@ -0,0 +1,22 @@
+package com.kh.project.repository;
+
+import java.util.List;
+
+import com.kh.project.entity.ReviewDto;
+import com.kh.project.entity.RoomDto;
+import com.kh.project.vo.ReviewVo;
+
+public interface ReviewDao {
+	// 리뷰 등록 
+	void insertReview(ReviewDto reviewDto);
+	// 모든 리뷰 목록
+	List<ReviewDto> list();
+	// 상세보기
+	ReviewVo detail(int reviewNo);
+	// 리뷰 수정
+	void edit(ReviewDto reviewDto);
+	// 한숙소에 대한 리뷰목록
+	List<ReviewDto> onelist(int roomNo);
+	// 리뷰 삭제
+	void delete(int reviewNo);
+}
